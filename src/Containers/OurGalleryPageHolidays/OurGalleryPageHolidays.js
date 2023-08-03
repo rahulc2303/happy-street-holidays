9e67884b389b81b4e4495c089f04922a02b30c85
Imakashkendre <akash.shopisthan@gmail.com>
2023-08-03 23:21:37 +0530

    video section added to gallery container

--- src/Containers/OurGalleryPageHolidays/OurGalleryPageHolidays.js
+++ src/Containers/OurGalleryPageHolidays/OurGalleryPageHolidays.js
@@ -67,6 +67,38 @@ const OurGalleryPageHolidays = () => {
       imageSrc: 'https://images.unsplash.com/photo-1461237439866-5a557710c921?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fHRvdXJzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
       imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
     },
+    {
+      id: 2,
+      name: 'Nomad Tumbler',
+      href: '#',
+      price: 'Kashmir',
+      imageSrc: 'https://images.unsplash.com/photo-1617829206657-ca53cbb6b0d4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHRvdXJzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+      imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
+    },
+    {
+      id: 3,
+      name: 'Focus Paper Refill',
+      href: '#',
+      price: 'Kerla',
+      imageSrc: 'https://images.unsplash.com/photo-1566865204669-c7b93be298bd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHRvdXJzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+      imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+    },
+    {
+      id: 4,
+      name: 'Machined Mechanical Pencil',
+      href: '#',
+      price: 'Rajasthan',
+      imageSrc: 'https://images.unsplash.com/photo-1461237439866-5a557710c921?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fHRvdXJzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+      imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+    },
+    {
+      id: 1,
+      name: '',
+      href: '#',
+      price: 'Rajasthan',
+      imageSrc: 'https://images.unsplash.com/photo-1571067295991-a359d382020d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dG91cnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
+      imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+    },
     // More products...
   ]
 
@@ -74,9 +106,6 @@ const OurGalleryPageHolidays = () => {
 
 
     <div className="flex-container-for-gallery">
-
-
-
 
 
       <div className="bg-white ">
@@ -87,7 +116,7 @@ const OurGalleryPageHolidays = () => {
               <h2 className="flex justify-center m-7 text-4xl">Our Gallery</h2>
 
             </div>
-            <div className="grid grid-cols-1 gap-x-5 gap-y-10 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 xl:gap-x-5 w-75% mb-7">
+            <div className="grid grid-cols-1 gap-x-5 gap-y-10 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 xl:gap-x-10 w-75% mb-7">
 
               {products.map((product) => (
                 <a key={product.id} href={product.href} className="group">
@@ -124,6 +153,15 @@ const OurGalleryPageHolidays = () => {
       </div>
     </div>
 
+    {/* video section */}
+    {/* <div className="">
+
+      <video className="h-full w-full rounded-lg" controls autoPlay muted>
+
+        <iframe width="75%" height="300" src="https://www.youtube.com/3f9d7805-6c23-4971-8e31-9de2410640a5" title="Travel to Jordan | Cinematic Video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      </video>
+
+    </div> */}
 
   </div>;
 };
