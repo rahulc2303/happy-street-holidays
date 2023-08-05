7611712cc603672f6e56ed8002435a05dc61d60f
Prasad Adhav <prasadadhav22@gmail.com>
2023-08-05 14:19:32 +0530

    added TravelVariationComponent

---
+++ src/Components/TravelVariationComponent/TravelVariationComponent.js
@@ -0,0 +1,79 @@
+import React from "react";
+import "./TravelVariationComponent.css";
+
+const TravelVariationComponent = () => {
+  return (
+    <>
+      {/* Travel variation component for the what we provide  */}
+
+      <div className="flex-container-for-the-variation-component">
+        {/* width set for the component 75em */}
+        <div className="width-specific-for-variation-component-75em">
+          {/* div contain img logo and info */}
+          <div className="div-for-logo-header-and-description">
+            {/* image */}
+            <div className="contains-img-for-component">
+              <img
+                src="https://preview.colorlib.com/theme/travelo/img/svg_icon/1.svg"
+                alt=""
+                srcset=""
+                className="img-for-the-section-variation-travel"
+              />
+              {/* header for the component */}
+              <p className="header-for-the-component-variation-travel">
+                Comfortable Journey
+              </p>
+              {/* description of the component */}
+              <p className="description-for-the-component-variation-travel ">
+                A wonderful serenity has taken to the possession of my entire
+                soul.
+              </p>
+            </div>
+          </div>{" "}
+          <div className="div-for-logo-header-and-description">
+            {/* image */}
+            <div className="contains-img-for-component">
+              <img
+                src="https://preview.colorlib.com/theme/travelo/img/svg_icon/1.svg"
+                alt=""
+                srcset=""
+                className="img-for-the-section-variation-travel"
+              />
+              {/* header for the component */}
+              <p className="header-for-the-component-variation-travel">
+                Comfortable Journey
+              </p>
+              {/* description of the component */}
+              <p className="description-for-the-component-variation-travel ">
+                A wonderful serenity has taken to the possession of my entire
+                soul.
+              </p>
+            </div>
+          </div>{" "}
+          <div className="div-for-logo-header-and-description">
+            {/* image */}
+            <div className="contains-img-for-component">
+              <img
+                src="https://preview.colorlib.com/theme/travelo/img/svg_icon/1.svg"
+                alt=""
+                srcset=""
+                className="img-for-the-section-variation-travel"
+              />
+              {/* header for the component */}
+              <p className="header-for-the-component-variation-travel">
+                Comfortable Journey
+              </p>
+              {/* description of the component */}
+              <p className="description-for-the-component-variation-travel ">
+                A wonderful serenity has taken to the possession of my entire
+                soul.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TravelVariationComponent;
