ca61723e55ead8f818a2e6e8213268e306e81b23
Prasad Adhav <prasadadhav22@gmail.com>
2023-08-05 22:27:18 +0530

    general changes

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <>
       {/* <NavBarHolidays /> */}
-      {/* <HomePageHolidays /> */}
-      <AboutPageHolidays />
+      <HomePageHolidays />
+      {/* <AboutPageHolidays /> */}
     </>
   );
 }
